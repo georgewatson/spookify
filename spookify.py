678c2f93031ab65f55aebc1c6d89362b8972b98a
George Watson <george.watson@york.ac.uk>
2018-11-01 16:23:26 +0000

    Functionalise for great good

--- spookify.py
+++ spookify.py
@@ -10,6 +10,19 @@ Usage:
 Spookifies all words over 3 characters.
 To force a match for words shorter than 3 characters, append some dots or
 something.
+
+Provides the following components:
+    main(name)
+        Returns a spookified Halloween version of the string 'name'
+    levenshtein(string1, string2)
+        Calculates the Levenshtein distance between two strings
+    SPOOKY_WORDS
+        A list of spooky words
+
+The above components are available for use in other code if imported correctly,
+and should have no side effects.
+However, Spookify is not at present packaged as a formal module and is provided
+as-is without any guarantee of safety or fitness for purpose.
 """
 
 # pylint: disable=consider-using-enumerate
@@ -22,32 +35,28 @@ import string
 def levenshtein(string1, string2):
     """
     Calculates the Levenshtein distance between two strings
-    See https://en.wikipedia.org/wiki/Levenshtein_distance
+    This is the minimum number of single-character changes (insertions,
+    deletions, and substitutions) required to transform one string into the
+    other.
+    See <https://en.wikipedia.org/wiki/Levenshtein_distance>
     """
-    # Only the last two rows of the matrix are actually necessary
+    # Only two rows of the matrix are actually necessary
     row1 = [None] * (len(string2) + 1)
     row2 = [None] * (len(string2) + 1)
 
     # Fill in row1 with the distance from an empty string1 (the length)
-    for i in range(len(row1)):
-        row1[i] = i
+    row1 = range(len(row1))
 
     # Calculate distances from previous row
     for i in range(len(string1)):
         # First element is empty string2, so use length again
         row2[0] = i + 1
 
+        # Calculate the minimum cost
         for j in range(len(string2)):
-            # Calculate costs
-            insertions = row2[j] + 1
-            deletions = row1[j+1] + 1
-            if string1[i] == string2[j]:
-                substitutions = row1[j]
-            else:
-                substitutions = row1[j] + 1
-
-            # Find the smallest number of modifications
-            row2[j+1] = min(insertions, deletions, substitutions)
+            row2[j+1] = min(row2[j] + 1,
+                            row1[j+1] + 1,
+                            row1[j] + (0 if string1[i] == string2[j] else 1))
 
         # Move down a row and repeat
         row1 = row2.copy()
@@ -56,9 +65,93 @@ def levenshtein(string1, string2):
     return row1[-1]
 
 
+def is_anagram(string1, string2):
+    """
+    Checks whether two strings contain the same characters
+    Returns True if so, else False
+    """
+    # Strings of different lengths can't be anagrams
+    if len(string1) != len(string2):
+        return False
+
+    characters1 = list(string1)
+    characters2 = list(string2)
+    characters1.sort()
+    characters2.sort()
+
+    return characters1 == characters2
+
+
+def score_substitution(word_part, possible_sub):
+    """
+    Determines the score of a substitution (lower is better)
+    Criteria:
+        Identical words score -1
+        Anagrams score         0
+        Substitutions that make the word shorter are never accepted
+            (they score word_length + 1, worse than any other substitution)
+        Other substitutions are given a score equal to their Levenshtein
+            distance divided by the length of the word
+    """
+    # If the words are the same, no substitution is needed
+    if possible_sub == word_part:
+        return -1
+
+    # Don't make the word shorter
+    if len(possible_sub) < len(word_part):
+        return len(word_part) + 1
+
+    # Accept anagrams immediately
+    if is_anagram(word_part, possible_sub):
+        return 0
+
+    # Otherwise, check the Levenshtein distance
+    # Divide by length to encourage longer subs
+    return levenshtein(possible_sub, word_part) / len(possible_sub)
+
+
+def best_substitution(word, possible_subs):
+    """
+    Finds the best possible substitution in a given word,
+    and returns the modified word
+    """
+    # Skip short words
+    ignored_words = ['and', 'for', 'the']
+    if len(word) < 3 or word in ignored_words:
+        return word
+
+    # Get all substrings of length >= 3
+    substrings = [word[i:j+1]
+                  for i in range(len(word) - 2)
+                  for j in range(i+2, len(word))]
+
+    # Sort by length to encourage longer substitutions
+    random.shuffle(substrings)
+    substrings.sort(key=len, reverse=True)
+
+    # Find the best spooky substitution
+    best_sub = min([(name_part,
+                     substitution,
+                     score_substitution(name_part, substitution))
+                    for name_part in substrings
+                    for substitution in possible_subs],
+                   key=lambda t: t[2])
+
+    # Substitute the relevant substring, delimited by hyphens
+    new_word = word.replace(best_sub[0], "-"+best_sub[1]+"-")
+    # But remove the hyphens at word boundaries
+    if new_word[0] == '-':
+        new_word = new_word[1:]
+    if new_word[-1] == '-':
+        new_word = new_word[:-1]
+
+    # Return the result
+    return new_word
+
+
 # A list of spooky words for potential substitutions
 SPOOKY_WORDS = ["halloween",
-                "axe", "axeman", "axes", "axewound",
+                "axe", "axes", "axewound",
                 "banshee", "banshees",
                 "bat", "bats",
                 "beast", "beastly", "beasts",
@@ -205,59 +298,17 @@ def main(name):
     See 'spookify' module docstring for more info.
     """
 
-    # Convert all strings to lowercase
+    # Convert strings to lowercase
     name = name.lower()
-    new_name = ""
 
-    # Randomly shuffle the spooky words for variety, then sort by length to
-    # encourage longer substitutions
+    # Randomly shuffle the spooky words for variety,
+    # then sort by length to encourage longer substitutions
     random.shuffle(SPOOKY_WORDS)
     SPOOKY_WORDS.sort(key=len, reverse=True)
 
-    # For each word in the name...
-    for word in name.split():
-        if len(word) < 3 or word in ['and', 'for', 'the']:
-            new_name = new_name + " " + word
-            continue
-
-        min_score = len(word) + 1
-        new_word = word
-        best_sub = ()
-
-        # Get all substrings of length >= 3
-        substrings = []
-        for i in range(len(word)):
-            for j in range(i, len(word)):
-                if len(word[i:j+1]) >= 3:
-                    substrings.append(word[i:j+1])
-        # Sort by length, to encourage longer substitutions
-        random.shuffle(substrings)
-        substrings.sort(key=len, reverse=True)
-
-        # Compare each substring to each spooky word
-        for name_part in substrings:
-            for spooky_word in SPOOKY_WORDS:
-                # Don't make the word shorter
-                if len(spooky_word) >= len(name_part):
-                    # Record the best match found so far
-                    this_levenshtein = levenshtein(spooky_word, name_part)
-                    this_score = this_levenshtein / len(spooky_word)
-                    # Strictly less-than to encourage longer subs
-                    if this_score < min_score:
-                        min_score = this_score
-                        best_sub = (name_part, spooky_word)
-
-        # Substitute the relevant substring, delimited by hyphens
-        if new_word:
-            new_word = word.replace(best_sub[0], "-"+best_sub[1]+"-")
-            # But remove the hyphens at word boundaries
-            if new_word[0] == '-':
-                new_word = new_word[1:]
-            if new_word[-1] == '-':
-                new_word = new_word[:-1]
-
-        # Add this word to the name
-        new_name = new_name + " " + new_word
+    # Construct a new name by applying the best substitution to each word
+    new_name = " ".join([best_substitution(word, SPOOKY_WORDS)
+                         for word in name.split()])
 
     return string.capwords(new_name)
 
